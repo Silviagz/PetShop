819906c875ec20ef4abf22ec48206c090aee9e61
Cearjeyou <harturista@gmail.com>
2022-07-27 17:20:02 -0500

    Subiendo archivo de la categoria comederos

--- root/JavaScript/main.js
+++ root/JavaScript/main.js
@@ -1,1 +1,23 @@
+let stockComederos = [
+    { id: 1, nombre: "Bebedero agua", tipo: "comedero", cantidad: 1, descripcion: "Comederos geniales para tu mascota", precio: 100000, img: './img/comederos/bebedero-agua.png' },
+    { id: 2, nombre: "Comedero gato", tipo: "comedero", cantidad: 1, descripcion: "Comederos geniales para tu mascota", precio: 150000, img: './img/comederos/comedero-gato.png'},
+    { id: 3, nombre: "Comedero hamster", tipo: "comedero", cantidad: 1, descripcion: "Comederos geniales para tu mascota", precio: 170000, img: './img/comederos/comedero-hamster.png'},
+    { id: 4, nombre: "Comedero perro", tipo: "comedero", cantidad: 1, descripcion: "Comederos geniales para tu mascota", precio: 180000, img: './img/comederos/comedero-perro.png'},
+];
 
+
+/* Generando las i-cards para los productos dependiendo de las categorias*/
+const contenedorComederos = document.getElementById('contenedorcomederos')
+
+stockComederos.forEach((comedero) => {
+    const div = document.createElement('div')
+    div.classList.add('producto')
+    div.innerHTML = `
+    <img src=${comedero.img} alt="Comedero">
+    <h3>${comedero.nombre}
+    <p>${comedero.descripcion}</p>
+    <p class="precioProducto">Precio:$ ${comedero.precio}</p>
+    <button id="agregar${comedero.id}" class="boton-agregar">Agregar <i class="fa-regular fa-heart"></i></button>
+    `
+    contenedorComederos.appendChild(div)
+});
---
+++ root/JavaScript/stock.js
@@ -0,0 +1,18 @@
+let stockComederos = [
+    { id: 1, nombre: "bebedero agua", tipo: "comedero", cantidad: 1, descripcion: "Comederos geniales para tu mascota", precio: 100000, img: '../img/comederos/bebedero-agua.png' },
+    { id: 2, nombre: "comedero gato", tipo: "comedero", cantidad: 1, descripcion: "Comederos geniales para tu mascota", precio: 150000, img: '../img/comederos/comedero-gato.png'},
+    { id: 3, nombre: "comedero hamster", tipo: "comedero", cantidad: 1, descripcion: "Comederos geniales para tu mascota", precio: 170000, img: '../img/comederos/comedero-hamster.png'},
+    { id: 4, nombre: "comedero perro", tipo: "comedero", cantidad: 1, descripcion: "Comederos geniales para tu mascota", precio: 180000, img: '../img/comederos/comedero-perro.png'},
+];
+let accesorios = [
+    { id: 1, nombre: "cama perro", tipo: "accesorio", cantidad: 1, descripcion: "Accesorios geniales para tu mascota", precio: 190000, img: '../img/accesorios/cama-perro.png'},
+    { id: 2, nombre: "cascabel gatos", tipo: "accesorio", cantidad: 1, descripcion: "Accesorios geniales para tu mascota", precio: 200000, img: '../img/accesorios/cascabel-gatos.png'},
+    { id: 3, nombre: "pechera perro", tipo: "accesotio", cantidad: 1, descripcion: "Accesorios geniales para tu mascota", precio: 250000, img: '../img/accesorios/pechera-perro.png'},
+    { id: 4, nombre: "rascador gato", tipo: "accesorio", cantidad: 1, descripcion: "Accesorios geniales para tu mascota", precio: 280000, img: '../img/accesorios/rascador-gato.png'},
+];
+let comida = [
+    { id: 1, nombre: "agility gold perro", tipo: "comida", cantidad: 1, descripcion: "Comida genial para tu mascota", precio: 300000, img: '../img/comida/AGILITY-GOLD-perro.png'},
+    { id: 2, nombre: "comida gato", tipo: "comida", cantidad: 1, descripcion: "Comida genial para tu mascota", precio: 350000, img: '../img/comida/comida-gato.png'},
+    { id: 3, nombre: "comida pájaro", tipo: "comida", cantidad: 1, descripcion: "Comida genial para tu mascota", precio: 380000, img: '../img/comida/Comida-pajaro.png'},
+    { id: 4, nombre: "comida peces", tipo: "comida", cantidad: 1, descripcion: "Comida genial para tu mascota", precio: 385000, img: '../img/comida/comida-peces.png'},
+]
