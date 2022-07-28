d03b536c5bdd7671276939cdaf171208451cf0fa
YuriBermudezRios <yuri2894@gmail.com>
2022-07-27 23:45:57 -0500

    Avance Carrito

--- root/JavaScript/main.js
+++ root/JavaScript/main.js
@@ -22,10 +22,69 @@ stockComederos.forEach((comedero) => {
     contenedorComederos.appendChild(div)
 });
 
+
+
+/* INICIO DE PROGRAMACIÓN CARRITO DE COMPRAS*/
 /*Creación del carrito de compras */
 let carrito = [];
 
 const agregarAlCarrito = (prodId) =>{
-    const item =stockProductos.find((prod) => prod.id ===prodId)
-    carrito.push(item)
+    const item =stockComederos.find((comederos) => comederos.id === prodId) /*Recorre el array StockComederos*/
+    carrito.push(item) /* Agregar productos seleccionados al carrito, es decir , a la array carrito de la fila 29*/
+    actualizarCarrito()
+    console.log(carrito)
 }
+
+/*Actualizar carrito cuando se agreguen productos */
+const contenedorCarrito = document.getElementById('carrito-contenedor')
+
+const actualizarCarrito =() => {
+    contenedorCarrito.innerHTML = ""
+
+    carrito.forEach((prod) =>{
+        const div = document.createElement('div')
+        div.className = ('productoEnCarrito')
+        div.innerHTML = `
+        <p>${comedero.nombre}</p>
+        <p>Precio: ${comedero.precio}</p>
+        <p>Cantidad: <span id="cantidad">${comedero.cantidad}</span></p>
+        <button onclick= "eliminarDelCarrito(${comederos.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></button>
+        `
+        contenedorCarrito.appendChild(div)
+
+        localStorage.setItem('carrito', JSON.stringify(carrito) )
+    })
+
+    contadorCarrito.innerText = carrito.length /* Para que en el carrito se vea la cantidad de articulos*/
+    precioTotal.innerText = carrito.reduce((acc, comederos) => acc + comederos.precio, 0)
+}
+
+/* Actualizar carrito cuando se eliminan productos */
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((comederos) => comederos.id=== prodId)
+    const indice = carrito.indexOf(item)
+    carrito.splice(indice, 1)
+    actualizarCarrito()
+}
+
+
+/* Funcionamiento del boton Vaciar Carrito*/
+const botonVaciar = document.getElementById('vaciar-carrito')
+
+botonVaciar.addEventListener('click', () =>{
+    carrito.length = 0
+    actualizarCarrito()
+})
+
+/* Contador del carrito, es decir que sume todos los artículos que se van adicionando al carrito*/
+const contadorCarrito = document.getElementById('contadorCarrito')
+const precioTotal = document.getElementById('precioTotal')
+
+/* Llamamos el local storage, esto con el fin de almacenar los datos desde Javascript*/
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        actualizarCarrito()
+    }
+})
+
